4e8d2c9cb55c5dd948fcf10046ee34b8d306e3a8
Omer Moran <imomerm@gmail.com>
2020-06-02 18:58:55 +0300

    First commit!

---
+++ Eventster/Eventster/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Eventster.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
