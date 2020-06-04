8289007a082be4aaf96ab34367f4e903c7e5fd12
Omer Moran <imomerm@gmail.com>
2020-06-04 19:01:45 +0300

    Models & DB files added

---
+++ Eventster/Eventster/Models/Booking.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Eventster.Models
+{
+    public class Booking
+    {
+        [Display(Name = "Client Id")]
+        [Required]
+        public string ClientId { get; set; }
+
+        [Display(Name = "Concert Id")]
+        [Required]
+        public int ConcertId { get; set; }
+
+        [Display(Name = "Ticket Id")]
+        [Required]
+        public int TicketId { get; set; }
+
+        [Display(Name = "Number of Tickets")]
+        [Range(1, 10)]
+        [Required]
+        public int TicketsNumber { get; set; }
+
+        public Concert Concert { get; set; }
+        public Ticket Ticket{ get; set; }
+        public Client Client { get; set; }
+    }
+}
