3e93d81a7d8bc4b695ff2590688b2bb8927a3d07
shahaf940 <shahaf940@DESKTOP-RIBE69H>
2020-08-04 20:19:08 +0300

    Fix routing of ticket actions

--- Eventster/Eventster/Controllers/TicketsController.cs
+++ Eventster/Eventster/Controllers/TicketsController.cs
@@ -66,7 +66,7 @@ namespace Eventster.Controllers
         public IActionResult Create()
         {
             ViewData["ConcertId"] = new SelectList(_context.Concert, "Id", "Name");
-            ViewData["TicketTypeId"] = new SelectList(_context.TicketType, "Id", "Name");
+            ViewData["TicketTypeId"] = new SelectList(_context.TicketType, "Id", "Type");
             return View(new Ticket());
         }
 
@@ -107,7 +107,7 @@ namespace Eventster.Controllers
                 return NotFound();
             }
             ViewData["ConcertId"] = new SelectList(_context.Concert, "Id", "Id", ticket.ConcertId);
-            ViewData["TicketTypeId"] = new SelectList(_context.TicketType, "Id", "Name", ticket.TicketTypeId);
+            ViewData["TicketTypeId"] = new SelectList(_context.TicketType, "Id", "Type", ticket.TicketTypeId);
             return View(ticket);
         }
 
