e115020f638e67fb3b10eda445b9b616ab949832
shahaf.shavit940@gmail.com <shahaf940@DESKTOP-RIBE69H>
2020-08-07 10:37:12 +0300

    Twitting

---
+++ Eventster/Eventster/Models/Tweet.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Eventster.Models
+{
+    public class Tweet
+    {
+        public string tweets { get; set; }
+    }
+}
